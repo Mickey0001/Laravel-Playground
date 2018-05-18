763a2ff183b460250d053257c7ec0056c6e92ecc
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-18 15:36:06 -0400

    Controller and composer

--- laravel-app/app/Http/Controllers/PagesController.php
+++ laravel-app/app/Http/Controllers/PagesController.php
@@ -7,14 +7,22 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index() {
-        return view('pages.index');
+        $title = 'Welcome!';
+        // return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
     }
 
     public function about() {
+        $title = 'About';
         return view('pages.about');
+        return view('pages.index')->with('title', $title);
     }
 
     public function services() {
-        return view('pages.services');
+        $data = array (
+            'title' => 'Services',
+            'services' => ['Design', 'Development', 'SEO']
+        );
+        return view('pages.services')->with($data);
     }
 }
