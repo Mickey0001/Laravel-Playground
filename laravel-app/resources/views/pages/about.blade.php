f88ecdffa415f70e3e8ef13b24b38e674699a276
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-18 13:28:50 -0400

    Add Blade templates

--- laravel-app/resources/views/pages/about.blade.php
+++ laravel-app/resources/views/pages/about.blade.php
@@ -1,17 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{config('app.name', 'Laravel')}}</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    </head>
-    <body>
+@extends('layouts.app')
+@section('content')
       <h1>About</h1>
       <p>This is the amazing about page.</p>
-    </body>
-</html>
+@endsection
