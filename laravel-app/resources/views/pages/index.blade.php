4e835b7b950c13105775157b2556035e1e527a73
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-18 16:24:39 -0400

    Custom (s)CSS and Nav edits

--- laravel-app/resources/views/pages/index.blade.php
+++ laravel-app/resources/views/pages/index.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 @section('content')
-      <h1>{{$title}}</h1>
-      <p>Laravel App for learning purposes.</p>
-      @endsection
+      <div class="jumbotron text-center">
+      <h1>Welcome to my Laravel app!</h1>
+      <p>Just a simple app for learning.</p>
+      <p><a href="" role="button" class="btn btn-primary">Login</a> <a href=""  role="button"  class="btn btn-success">Register</a></p>
+      </div>      
+@endsection
