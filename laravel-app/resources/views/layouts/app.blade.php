3fad469671e8c7d0bc27868632d1a5b2dcee5961
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-18 15:56:04 -0400

    Views

--- laravel-app/resources/views/layouts/app.blade.php
+++ laravel-app/resources/views/layouts/app.blade.php
@@ -6,11 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name', 'Laravel')}}</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
     <body>
-      @yield('content')
+        <div class="container">
+           @yield('content')
+        </div>
     </body>
 </html>
