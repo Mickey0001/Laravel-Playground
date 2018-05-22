37db79e2207696605bb53930d2180e7f967d03d2
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-22 09:55:58 -0400

    PostsController update

--- laravel-app/app/Http/Controllers/PostsController.php
+++ laravel-app/app/Http/Controllers/PostsController.php
@@ -87,7 +87,14 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
+
         $post = Post::find($id);
+
+        //Check for correct user
+        if(auth()->user()->id !== $post->user_id){
+            return redirect('posts')->with('error', 'You are not authorized.');
+        }
+
         return view('posts.edit')->with('post', $post);
     }
 
@@ -123,6 +130,12 @@ class PostsController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+
+        //Check for correct user
+        if(auth()->user()->id !== $post->user_id){
+            return redirect('posts')->with('error', 'You are not authorized.');
+        }
+
         $post->delete();
         return redirect('/posts')->with('success', 'Post Deleted');
     }
--- laravel-app/resources/views/posts/show.blade.php
+++ laravel-app/resources/views/posts/show.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  <p><button class="btn btn-default"><a href="/posts">Back</a></button><br></p>
-    <h2>{{$post->title}}</h2>
-    <div>
-      {!!$post->body!!}
-    </div>
+    <p><button class="btn btn-default"><a href="/posts">Back</a></button><br></p>
+      <h2>{{$post->title}}</h2>
+      <div>
+        {!!$post->body!!}
+      </div>
+      <hr>
+    <small>Posted on {{$post->created_at}} by {{$post->user->name}}</small>
     <hr>
-  <small>Posted on {{$post->created_at}} by {{$post->user->name}}</small>
-  <hr>
-  <button class="btn btn-default">  <a href="/posts/{{$post->id}}/edit" >Edit post</a></button>
-
-
-  {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
-    {{Form::hidden('_method', 'DELETE')}}
-    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-  {!!Form::close()!!}
-
+    @if(!Auth::guest())
+      @if(Auth::user()->id == $post->user_id)
+        <button class="btn btn-default">  <a href="/posts/{{$post->id}}/edit" >Edit post</a></button>
+        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+          {{Form::hidden('_method', 'DELETE')}}
+          {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+        {!!Form::close()!!}
+      @endif
+    @endif
   @endsection
