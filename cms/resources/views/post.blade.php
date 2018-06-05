11527524145991a35a926c0e2feaf2ae67e27af0
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-05 15:43:16 -0400

    Layout templating

--- cms/resources/views/post.blade.php
+++ cms/resources/views/post.blade.php
@@ -1,15 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>CMS</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <!-- Styles --> 
-    </head>
-    <body>
+@extends('layouts.app')
+
+@section('content')
     <h1>Post {{$id}} {{$name}}</h1>
-    </body>
-</html>
+@endsection()
--- cms/resources/views/welcome.blade.php
+++ cms/resources/views/welcome.blade.php
@@ -1,19 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>CMS</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-   
-    </head>
-    <body>
-        <h1>CMS</h1>
-    </body>
-</html>
+@section('content')
+    <h1>CMS</h1>
+@endsection()
