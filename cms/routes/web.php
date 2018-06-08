f50d7ce2068048fc2b2b44477b1973cc417f2e6f
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-08 17:49:24 -0400

    SQL PDO stuff

--- cms/routes/web.php
+++ cms/routes/web.php
@@ -36,10 +36,15 @@ Route::get('/', function(){
 
 // });
 
-Route::get('/update', function(){
-  
-});
+// Route::get('/update', function(){
+//   $updated = DB::update('update posts set title = "Updated title" where id = ?', [1]);
+//   return $updated;
+// });
 
+Route::get('/delete', function(){
+  $deleted = DB::delete('delete from posts where id = ?', [1]);
+  return $deleted;
+});
 
 
 
