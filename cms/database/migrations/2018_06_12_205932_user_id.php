37a7607be7a36a077c7183f2f162fe37582c1f88
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-12 17:03:41 -0400

    Relations

--- cms/database/migrations/2018_06_12_205932_user_id.php
+++ cms/database/migrations/2018_06_12_205932_user_id.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class UserId extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,7 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->string('title');
-            $table->text('body');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -29,6 +23,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        //
     }
 }
