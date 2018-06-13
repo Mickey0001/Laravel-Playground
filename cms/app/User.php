8d7b2549ab16f3e0852553bd24fbce058c16c989
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-13 18:05:29 -0400

    Relations

--- cms/app/User.php
+++ cms/app/User.php
@@ -34,4 +34,8 @@ class User extends Authenticatable
     public function posts(){
         return $this->hasMany('App\Post');
     }
+
+    public function roles(){
+        return $this->belongsToMany('App\Role');
+    }
 }
