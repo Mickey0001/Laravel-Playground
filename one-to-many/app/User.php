ba1124eee4737e78bcef8343c330fa6f8c3105ba
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-18 18:14:27 -0400

    Init

--- one-to-many/app/User.php
+++ one-to-many/app/User.php
@@ -26,7 +26,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    public function address(){
-        return $this->hasOne('App\Address');
-    }
 }
