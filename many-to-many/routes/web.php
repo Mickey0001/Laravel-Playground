ce385e20185cf4098187947babff5ed2bdcf6d03
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-19 16:57:29 -0400

    CRUD shit

--- many-to-many/routes/web.php
+++ many-to-many/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Role;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +16,40 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function(){
+    $user = User::find(1);
+
+    $user->roles()->save(new Role(['name'=>'Administrator']));
+});
+
+Route::get('/read', function(){
+    $user = User::findOrFail(1);
+        return $user;
+});
+
+Route::get('/update', function(){
+    $user = User::findOrFail(1);
+
+    if($user->has('/roles')){
+        foreach($user->roles as $role){
+
+            if($role->name =='Administrator'){
+
+                $role->name = "subscriber";
+
+                $role->save();
+            }
+        }
+    }
+});
+
+
+Route::get('/delete', function(){
+    $user = User::findOrFail(1);
+
+    foreach($user->roles as $role){
+        $role->whereId(2)->delete();
+    }
 });
