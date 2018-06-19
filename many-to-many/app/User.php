9d0f81a411c49afd4af0c94a7c4c7accef4f3353
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-19 14:25:52 -0400

    Init

--- many-to-many/app/User.php
+++ many-to-many/app/User.php
@@ -26,8 +26,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function posts(){
-        return $this->hasMany('App\Post');
-    }
 }
