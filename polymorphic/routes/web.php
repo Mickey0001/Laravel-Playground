6290addadd741cbf74314acc8a01c41629a7b882
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-21 08:22:42 -0400

    Routes

--- polymorphic/routes/web.php
+++ polymorphic/routes/web.php
@@ -1,4 +1,6 @@
 <?php
+use App\Staff;
+use App\Photo;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +16,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('create', function () {
+    $staff = Staff::find(1);
+
+    $staff->photos()->create(['example.jpg']);
+});
+
