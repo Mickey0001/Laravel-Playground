1d6987147b871018e1a08b527d849829f156a427
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-22 15:18:12 -0400

    Tables and routes

--- polymorphicmanytomany/database/migrations/2018_06_22_184203_create_tags_table.php
+++ polymorphicmanytomany/database/migrations/2018_06_22_184203_create_tags_table.php
@@ -15,7 +15,7 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
--- polymorphicmanytomany/routes/web.php
+++ polymorphicmanytomany/routes/web.php
@@ -20,16 +20,16 @@ Route::get('/', function () {
 });
 
 Route::get('create', function () {
+
     $post = Post::create(['name'=>'My Fist post.']);
 
     $tag1 = Tag::find(1);
-
-    $post->tags()-save($tag1);
+    $post->tags()->save($tag1);
 
     $video = Video::create(['name'=>'video.mov']);
 
     $tag2 = Tag::find(2);
-
-    $video->tags()-save($tag2);
+    $video->tags()->save($tag2);
 });
+
 
