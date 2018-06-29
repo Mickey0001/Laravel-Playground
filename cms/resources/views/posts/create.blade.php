293bca253af5704f99de65ae2810ff3f99079d85
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-29 03:11:45 +0200

    Create post form

--- cms/resources/views/posts/create.blade.php
+++ cms/resources/views/posts/create.blade.php
@@ -3,10 +3,19 @@
 @section('content')
 
   <h1>Create Post</h1>
-  <form method="post" action="/posts">
-    <input type="text" name="title" placeholder="Enter Title">
-    <input type="submit" name="submit">
-  </form>
+  {!! Form::open{['method'=>'Post', 'action'=>'PostsController@store']} !!}
+
+<div class="form-group">
+  {{!! Form::label('title', 'Title:') !!}}
+  {{!! Form::text('title', null, ['class'=>'form-control']) !!}}
+</div>
+
+<div class="form-group">
+  {{!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}}
+</div>
+
+{{!! Form::close() !!}}
+
 
   @if(count($errors) > 0)
     <div class="alert alert-danger">
