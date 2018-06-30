aac978ef1c7731f0f0334f56a385b0bbb3fa2540
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-30 04:49:55 +0200

    Admin Middleware

--- middleware/app/Http/Middleware/IsAdmin.php
+++ middleware/app/Http/Middleware/IsAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -18,7 +19,7 @@ class IsAdmin
         $user = Auth::user();
 
         if($user->isAdmin()){
-           return redirect()->intended('/admin');
+           return redirect('/');
         }
         return $next($request);
     }
