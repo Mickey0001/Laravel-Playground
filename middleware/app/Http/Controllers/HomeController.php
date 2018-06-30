cb37b9ff1d3b62363b6b13aace39dfab587cbdf3
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-30 17:35:53 +0200

    Sessions

--- middleware/app/Http/Controllers/HomeController.php
+++ middleware/app/Http/Controllers/HomeController.php
@@ -21,8 +21,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        // $request->session(['mirza'=>'student']);
+
+        $request->session()->get('mirza');
         return view('home');
     }
 }
