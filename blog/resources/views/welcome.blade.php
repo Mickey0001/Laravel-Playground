d0553219993dc5158f5385237d00ec6dadd28040
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-02 03:10:50 +0200

    Routes and views

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -79,11 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laracasts
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="/about">About</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
