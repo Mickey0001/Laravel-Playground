e03d8d19e9f72d2f7403daf97e8cf9ade008bc1a
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-02 03:27:26 +0200

    Refresh migrations

--- blog/database/migrations/2018_07_02_012531_create_tasks_table.php
+++ blog/database/migrations/2018_07_02_012531_create_tasks_table.php
@@ -15,7 +15,6 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
             $table->string('title');
             $table->text('body');
             $table->timestamps();
