453130e0c26721f162a545013a3e301474b3405a
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-02 18:53:46 +0200

    View

--- blog/resources/views/tasks/show.blade.php
+++ blog/resources/views/tasks/show.blade.php
@@ -7,10 +7,8 @@
   <title>Document</title>
 </head>
 <body>
-  <ul>
-    @foreach ($tasks as $task)
-        <li>{{ $task->body }}</li>
-    @endforeach
-</ul>
+
+      <h1>{{ $task->body }}</h1>
+
 </body>
 </html>
