17f35745004b3c76cab92362a6405612b8de7e2d
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-02 23:04:13 +0200

    Layouts

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -1,5 +1,7 @@
 @extends('layout')
 
 @section('content')
+ 
+  <h1>Place to show the posts</h1>
 
 @endsection
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -4,4 +4,5 @@ use App\Task;
 
 Route::get('/', 'PostsController@index');
 
+Route::get('posts/{post}', 'PostsController@show');
 
