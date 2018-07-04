2f9ed18ea46e6bed96a40aa5012564aaef6566c3
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-05 00:34:52 +0200

    Extending the Model

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'body'];
+    // //Whitelisted input fields
+    // protected $fillable = ['title', 'body'];
+
+    // //Blacklisted input fields 
+    // protected $guarded = ['title', 'body'];
 }
