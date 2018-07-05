03e879dc8bfcc21a89cccd5c3b3e32686a08b773
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-05 23:07:17 +0200

    Carbon dates

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -14,6 +14,30 @@
         {{ $post->body}}
       
     </p>
+<hr>
+    <div class="comments">
+
+        <ul class="list-group">
+
+            @foreach($post->comments as $comment)
+
+                <li class="list-group-item">
+
+                    <strong>
+
+                            {{ Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}
+
+                    </strong>
+
+                    {{ $comment->body }}
+                </li>
+
+            @endforeach
+
+        </ul>
+<br>
+    </div>
+
 </div>
 
 @endsection
