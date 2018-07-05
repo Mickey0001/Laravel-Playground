b2592d5aada2f96c1e7741ab5995b6e8a93f7764
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-05 21:16:52 +0200

    Posts

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -10,7 +10,9 @@ class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
     }
 
     public function show()
