dc45252e8046b6cd57caffb2f27ca700de1b3a7b
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-05 21:52:59 +0200

    Posts

---
+++ blog/resources/views/posts/post.blade.php
@@ -0,0 +1,9 @@
+<div class="blog-post">
+
+  <h2 class="blog-post-title">{{ $post->title }}</h2>
+
+        <p class="blog-post-meta">{{{ $post->create_at }}} <a href="#">Mark</a></p>
+
+        {{ $post->body}}
+
+</div><!-- /.blog-post -->
