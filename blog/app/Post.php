4b0f93950bd2edb577cf7b0006eb7176a260c0ae
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-06 05:04:22 +0200

    Post

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -4,23 +4,20 @@ namespace App;
 
 class Post extends Model
 {
-    // //Whitelisted input fields
     protected $fillable = ['title', 'body'];
-
-    // //Blacklisted input fields 
-    // protected $guarded = ['title', 'body'];
 
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function addComment($body)
     {
         $this->comments()->create(compact('body'));
-        // Comment::create([
-        //     'body' => $body,
-        //     'post_id' =>  $this->id
-        // ]);
     }
 }
