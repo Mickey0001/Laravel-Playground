2db785535147dd33b4bfa5dad67aa5a7bfe220ca
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-10 23:09:19 +0200

    Register

--- social/database/migrations/2018_07_10_205132_create_users_table.php
+++ social/database/migrations/2018_07_10_205132_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->timestamps();
             $table->string('email');
+            $table->string('name');
             $table->string('password');
             $table->rememberToken();
         });
