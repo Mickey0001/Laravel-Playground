4bc323382d711a67619b91f4253bf5b3bdbf57b2
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-13 02:10:40 +0200

    Routes

--- codehacking/app/Http/Controllers/AdminUsersController.php
+++ codehacking/app/Http/Controllers/AdminUsersController.php
@@ -13,7 +13,7 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.users.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.users.create');
     }
 
     /**
@@ -45,7 +45,7 @@ class AdminUsersController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('admin.users.show');
     }
 
     /**
@@ -56,7 +56,7 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('admin.users.edit');
     }
 
     /**
