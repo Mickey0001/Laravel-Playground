a22ca174fa9754d6f932f088ea7cad398ef414ac
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-18 02:16:17 +0200

    user request

--- codehacking/app/Http/Controllers/AdminUsersController.php
+++ codehacking/app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
+
 use Illuminate\Http\Request;
 
 use App\User;
@@ -40,7 +42,7 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
         return $request->all();
     }
