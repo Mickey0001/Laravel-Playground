6ae0792bfda3e9dda9010aa79f9f50634c1c30eb
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-19 22:04:42 +0200

    Middleware

--- codehacking/routes/web.php
+++ codehacking/routes/web.php
@@ -23,4 +23,10 @@ Route::get('admin', function(){
     return view('admin.index');
 });
 
-Route::resource('admin/users', 'AdminUsersController', ['as' => 'admin']);
+Route::group(['middleware' => 'admin'], function(){
+
+    Route::resource('admin/users', 'AdminUsersController', ['as' => 'admin']);
+
+});
+
+
