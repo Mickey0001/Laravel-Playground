324c8bcb2ebf9bcfa3cc31dc9881829b83e06e2c
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-22 16:45:53 +0200

    posts table

--- codehacking/database/migrations/2018_07_22_144313_create_posts_table.php
+++ codehacking/database/migrations/2018_07_22_144313_create_posts_table.php
@@ -15,7 +15,13 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('category_id')->unsigned()->index();
+            $table->integer('photo_id')->unsigned()->index();
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
+
         });
     }
 
