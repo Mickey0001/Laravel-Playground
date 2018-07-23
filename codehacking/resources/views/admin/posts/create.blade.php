9d0c8e5ae4b2a07ad02773aa12adc8936606894b
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-23 02:36:11 +0200

    Create post

--- codehacking/resources/views/admin/posts/create.blade.php
+++ codehacking/resources/views/admin/posts/create.blade.php
@@ -4,4 +4,17 @@
 
   <h1>New Post</h1>
 
+  {!! Form::open(['method'=>'POST', 'action'=>'AdminPostsController@store']) !!}
+
+  <div class="form-group">
+    {!! Form::label('title','Title:') !!}
+    {!! Form::text('title', null,  ['class'=>'form-control']) !!}
+  </div>
+
+  <div class="form-group">
+    {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+  </div>
+
+{!! Form::close() !!}
+
 @endsection
