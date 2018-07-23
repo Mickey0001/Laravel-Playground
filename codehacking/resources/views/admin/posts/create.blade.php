b5c663aad2c1c5ec3c42a619cc05898f076d3472
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-23 02:41:28 +0200

    create post

--- codehacking/resources/views/admin/posts/create.blade.php
+++ codehacking/resources/views/admin/posts/create.blade.php
@@ -6,14 +6,29 @@
 
   {!! Form::open(['method'=>'POST', 'action'=>'AdminPostsController@store']) !!}
 
-  <div class="form-group">
-    {!! Form::label('title','Title:') !!}
-    {!! Form::text('title', null,  ['class'=>'form-control']) !!}
-  </div>
+      <div class="form-group">
+        {!! Form::label('title','Title:') !!}
+        {!! Form::text('title', null,  ['class'=>'form-control']) !!}
+      </div>
 
-  <div class="form-group">
-    {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
-  </div>
+      <div class="form-group">
+        {!! Form::label('category_id','Category:') !!}
+        {!! Form::select('category_id', array(''=>'options'), null,  ['class'=>'form-control']) !!}
+      </div>
+
+      <div class="form-group">
+        {!! Form::label('title','Title:') !!}
+        {!! Form::file('title', ['class'=>'form-control']) !!}
+      </div>
+
+      <div class="form-group">
+        {!! Form::label('body','Description:') !!}
+        {!! Form::textarea('body', ['class'=>'form-control', 'rows'=>3]) !!}
+      </div>
+
+      <div class="form-group">
+        {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+      </div>
 
 {!! Form::close() !!}
 
