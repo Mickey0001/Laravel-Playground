859c47d22a09586de9e684b406d1086eac318aae
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-27 21:27:24 +0200

    Seeder

--- codehacking/database/seeds/UsersTableSeeder.php
+++ codehacking/database/seeds/UsersTableSeeder.php
@@ -11,6 +11,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert
+        ([
+            'name' => str_random(10),
+            'role_id'=> 2,
+            'photo_id'=> str_random(10),
+            'is_active' => 1,
+            'email'=> str_random(10).'@mirza.com',
+            'password'=> bcrypt('secret')
+        ]);
     }
 }
